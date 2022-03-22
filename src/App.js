894a80467ace1328c2cfcbdc074c04a7331ab86f
cristinarangoe <cristinarangoe@hotmail.com>
2022-03-22 18:57:23 -0500

    creating the react app

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let mystring = "hola mundo";
+  let nombre = "Mi nombre es Cristina Arango Escobar"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{mystring}</p>
+      <p>{nombre}</p>
     </div>
   );
 }
